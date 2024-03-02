726a238bf469373e28117592db5b29c99307aa70
sergeikrylovich <sergeybumbum@gmail.com>
2024-03-03 02:40:51 +0300

    add PracticeFormEnum

---
+++ src/test/java/ui/pages/forms/PracticeFormEnum.java
@@ -0,0 +1,22 @@
+package ui.pages.forms;
+
+public class PracticeFormEnum {
+
+    public enum Months {
+        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+    }
+
+    public enum Gender {
+        MALE, FEMALE, OTHER
+    }
+
+    public enum Hobbies {
+        SPORTS, READING, MUSIC
+    }
+
+    public enum States {
+        NCR, UTTAR_PRADESH, HARYANA, RAJASTHAN
+    }
+
+
+}
