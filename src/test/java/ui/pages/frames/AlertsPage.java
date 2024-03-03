c0f9425c71e685b7ead53b954cf6418e2a4e1a81
sergeikrylovich <sergeybumbum@gmail.com>
2024-03-03 19:30:29 +0300

    create page for Alerts and tests

--- src/test/java/ui/pages/frames/AlertsPage.java
+++ src/test/java/ui/pages/frames/AlertsPage.java
@@ -1,10 +1,60 @@
 package ui.pages.frames;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import ui.BasePage;
 
 public class AlertsPage extends BasePage {
+
+    private final By alertBtn = By.xpath("//button[@id='alertButton']");
+    private final By alertAfter5SecondsBtn = By.xpath("//button[@id='timerAlertButton']");
+    private final By confirmBtn = By.xpath("//button[@id='confirmButton']");
+    private final By promptBtn = By.xpath("//button[@id='promtButton']");
+
     public AlertsPage(WebDriver driver) {
         super(driver);
+    }
+
+    public AlertsPage clickOnAlertBtn() {
+        driver.findElement(alertBtn).click();
+        return this;
+    }
+    public AlertsPage clickOnAlertAfter5SecondsBtn() {
+        driver.findElement(alertAfter5SecondsBtn).click();
+        return this;
+    }
+
+    public AlertsPage clickOnConfirmBoxBtn() {
+        driver.findElement(confirmBtn).click();
+        return this;
+    }
+    public AlertsPage clickOnPromptBoxBtn() {
+        driver.findElement(promptBtn).click();
+        return this;
+    }
+
+    public void clickOnAcceptInAlert() {
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept();
+    }
+
+    public void clickOnDismissInAlert() {
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().dismiss();
+    }
+
+    public void fillAlertAndClickOnAccept(String prompt) {
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().sendKeys(prompt);
+        driver.switchTo().alert().accept();
+    }
+
+
+    public void fillAlertAndClickOnDismiss(String prompt) {
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().sendKeys("hello");
+        driver.switchTo().alert().dismiss();
     }
 }
---
+++ src/test/java/ui/tests/AlertsTests.java
@@ -0,0 +1,76 @@
+package ui.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import ui.annotations.ChromeSetUp;
+import ui.pages.MainPage;
+
+@ChromeSetUp
+public class AlertsTests {
+
+    private WebDriver driver;
+    private MainPage mainPage;
+
+
+    @BeforeEach
+    public void setUp() {
+        mainPage = new MainPage(driver);
+    }
+
+    @Test
+    public void clickOnAlertAndThenAcceptTest() throws InterruptedException {
+        mainPage.clickOnFrames()
+                .clickOnAlertsMenu()
+                .clickOnAlertBtn()
+                .clickOnAcceptInAlert();
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void clickOnAlertAndWait5SecondsThenAcceptTest() throws InterruptedException {
+        mainPage.clickOnFrames()
+                .clickOnAlertsMenu()
+                .clickOnAlertAfter5SecondsBtn()
+                .clickOnAcceptInAlert();
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void clickOnConfirmBoxThenAcceptTest() throws InterruptedException {
+        mainPage.clickOnFrames()
+                .clickOnAlertsMenu()
+                .clickOnConfirmBoxBtn()
+                .clickOnAcceptInAlert();
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void clickOnConfirmBoxThenDismissTest() throws InterruptedException {
+        mainPage.clickOnFrames()
+                .clickOnAlertsMenu()
+                .clickOnConfirmBoxBtn()
+                .clickOnDismissInAlert();
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void clickOnPromptBoxThenAcceptTest() throws InterruptedException {
+        mainPage.clickOnFrames()
+                .clickOnAlertsMenu()
+                .clickOnPromptBoxBtn()
+                .fillAlertAndClickOnAccept("LOLOLO");
+
+        Thread.sleep(3000);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        driver.quit();
+    }
+}
